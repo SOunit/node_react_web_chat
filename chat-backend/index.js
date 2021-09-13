f11e15de5af6999075f7e468c03031f297369119
SOunit <workbookwork7@gmail.com>
2021-09-12 23:51:01 -0700

    add nodemon

--- chat-backend/index.js
+++ chat-backend/index.js
@@ -1,1 +1,13 @@
-console.log('hello from node');
+const express = require('express');
+
+const app = express();
+
+app.get('/home', (req, res) => {
+  return res.send('home');
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server listening to port ${port}`);
+});
