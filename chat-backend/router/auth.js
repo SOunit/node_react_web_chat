2febe847eac96002ae55840a70c6ad76877cec91
SOunit <workbookwork7@gmail.com>
2021-09-13 16:37:31 -0700

    add express validator to request body

--- chat-backend/router/auth.js
+++ chat-backend/router/auth.js
@@ -1,8 +1,19 @@
 const router = require('express').Router();
 const { login, register } = require('../controllers/authController');
+const { body } = require('express-validator');
+
+router.post(
+  '/register',
+  [
+    body('firstName').notEmpty(),
+    body('lastName').notEmpty(),
+    body('gender').notEmpty(),
+    body('email').isEmail(),
+    body('password').isLength({ min: 5 }),
+  ],
+  register
+);
 
 router.post('/login', login);
-
-router.post('/register', register);
 
 module.exports = router;
