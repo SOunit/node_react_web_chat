1c54a0cfecceec35d717129af32099a49adce156
SOunit <workbookwork7@gmail.com>
2021-09-14 16:39:01 -0700

    add login component content

--- chat-frontend/src/components/Auth/Login.js
+++ chat-frontend/src/components/Auth/Login.js
@@ -1,5 +1,32 @@
+import LoginImage from '../../assets/images/login.svg';
+
 const Login = () => {
-  return <div>Login Screen</div>;
+  return (
+    <div>
+      <div id='auth-container'>
+        <div id='auth-card'>
+          <div>
+            <div id='image-section'>
+              <img src={LoginImage} alt='login' />
+            </div>
+            <div id='form-section'>
+              <h2>Welcome back</h2>
+              <form>
+                <div className='input-field'>
+                  <input placeholder='email' />
+                </div>
+                <div className='input-field'>
+                  <input placeholder='password' />
+                </div>
+                <button>LOGIN</button>
+              </form>
+              <p>Don't have an account? Register</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
