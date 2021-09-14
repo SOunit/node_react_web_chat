bb8ce8f437e07e756e2888f4521b7f305ba0a0dc
SOunit <workbookwork7@gmail.com>
2021-09-13 17:12:30 -0700

    add validator to login

--- chat-backend/router/auth.js
+++ chat-backend/router/auth.js
@@ -2,9 +2,10 @@ const router = require('express').Router();
 const { login, register } = require('../controllers/authController');
 const { validate } = require('../validators');
 const { rules: registrationRules } = require('../validators/auth/register');
+const { rules: loginRules } = require('../validators/auth/login');
 
 router.post('/register', [registrationRules, validate], register);
 
-router.post('/login', login);
+router.post('/login', [loginRules, validate], login);
 
 module.exports = router;
