bb7f8fa724889210fc24eed704fc56471b943a98
SOunit <workbookwork7@gmail.com>
2021-09-14 19:24:10 -0700

    add style to register page component

--- chat-frontend/src/components/Auth/Login.js
+++ chat-frontend/src/components/Auth/Login.js
@@ -1,29 +1,30 @@
-import LoginImage from '../../assets/images/login.svg';
+import { Link } from 'react-router-dom';
+import loginImage from '../../assets/images/login.svg';
 
 import './Auth.scss';
 
 const Login = () => {
   return (
-    <div>
-      <div id='auth-container'>
-        <div id='auth-card'>
-          <div className='card-shadow'>
-            <div id='image-section'>
-              <img src={LoginImage} alt='login' />
-            </div>
-            <div id='form-section'>
-              <h2>Welcome back</h2>
-              <form>
-                <div className='input-field mb-1'>
-                  <input placeholder='email' />
-                </div>
-                <div className='input-field mb-2'>
-                  <input placeholder='password' />
-                </div>
-                <button>LOGIN</button>
-              </form>
-              <p>Don't have an account? Register</p>
-            </div>
+    <div id='auth-container'>
+      <div id='auth-card'>
+        <div className='card-shadow'>
+          <div id='image-section'>
+            <img src={loginImage} alt='login' />
+          </div>
+          <div id='form-section'>
+            <h2>Welcome back</h2>
+            <form>
+              <div className='input-field mb-1'>
+                <input placeholder='email' />
+              </div>
+              <div className='input-field mb-2'>
+                <input placeholder='password' />
+              </div>
+              <button>LOGIN</button>
+            </form>
+            <p>
+              Don't have an account? <Link to='/register'>Register</Link>
+            </p>
           </div>
         </div>
       </div>
--- chat-frontend/src/components/Auth/Register.js
+++ chat-frontend/src/components/Auth/Register.js
@@ -1,5 +1,47 @@
+import { Link } from 'react-router-dom';
+import registerImage from '../../assets/images/register.svg';
+
+import './Auth.scss';
+
 const Register = () => {
-  return <div>Register Screen</div>;
+  return (
+    <div id='auth-container'>
+      <div id='auth-card'>
+        <div className='card-shadow'>
+          <div id='image-section'>
+            <img src={registerImage} alt='register' />
+          </div>
+          <div id='form-section'>
+            <h2>Create an account</h2>
+            <form>
+              <div className='input-field mb-1'>
+                <input placeholder='First name' />
+              </div>
+              <div className='input-field mb-1'>
+                <input placeholder='Lst name' />
+              </div>
+              <div className='input-field mb-1'>
+                <input placeholder='Email' />
+              </div>
+              <div className='input-field mb-1'>
+                <select>
+                  <option value='male'>Male</option>
+                  <option value='female'>Female</option>
+                </select>
+              </div>
+              <div className='input-field mb-2'>
+                <input placeholder='Password' />
+              </div>
+              <button>REGISTER</button>
+            </form>
+            <p>
+              Already have an account? <Link to='/login'>Login</Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
