586fab9a30e35e76ea94ff2fb627ae8cb2c37a46
SOunit <workbookwork7@gmail.com>
2021-09-15 15:48:50 -0700

    add http request to login page

--- chat-frontend/src/components/Auth/Login.js
+++ chat-frontend/src/components/Auth/Login.js
@@ -1,9 +1,29 @@
+import { useState } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import loginImage from '../../assets/images/login.svg';
 
 import './Auth.scss';
 
 const Login = () => {
+  const [email, setEmail] = useState('john.doe@gmail.com');
+  const [password, setPassword] = useState('secret');
+
+  const submitForm = (e) => {
+    e.preventDefault();
+
+    axios
+      .post(`/chat-backend/login`, { email, password })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    console.log({ email, password });
+  };
+
   return (
     <div id='auth-container'>
       <div id='auth-card'>
@@ -13,12 +33,24 @@ const Login = () => {
           </div>
           <div id='form-section'>
             <h2>Welcome back</h2>
-            <form>
+            <form onSubmit={submitForm}>
               <div className='input-field mb-1'>
-                <input placeholder='email' />
+                <input
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email}
+                  required='required'
+                  type='text'
+                  placeholder='email'
+                />
               </div>
               <div className='input-field mb-2'>
-                <input placeholder='password' />
+                <input
+                  onChange={(e) => setPassword(e.target.value)}
+                  value={password}
+                  required='required'
+                  type='password'
+                  placeholder='password'
+                />
               </div>
               <button>LOGIN</button>
             </form>
