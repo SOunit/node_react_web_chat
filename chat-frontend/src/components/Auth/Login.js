27cfaeba74e0b2dc4c4954ca9a2feae4abe79cb3
SOunit <workbookwork7@gmail.com>
2021-09-14 19:12:24 -0700

    add style to login page component

--- chat-frontend/src/components/Auth/Login.js
+++ chat-frontend/src/components/Auth/Login.js
@@ -7,17 +7,17 @@ const Login = () => {
     <div>
       <div id='auth-container'>
         <div id='auth-card'>
-          <div>
+          <div className='card-shadow'>
             <div id='image-section'>
               <img src={LoginImage} alt='login' />
             </div>
             <div id='form-section'>
               <h2>Welcome back</h2>
               <form>
-                <div className='input-field'>
+                <div className='input-field mb-1'>
                   <input placeholder='email' />
                 </div>
-                <div className='input-field'>
+                <div className='input-field mb-2'>
                   <input placeholder='password' />
                 </div>
                 <button>LOGIN</button>
