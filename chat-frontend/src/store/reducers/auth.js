5b0f1573d28d94888242bd1ecaa797e278484f29
SOunit <workbookwork7@gmail.com>
2021-09-16 17:09:09 -0700

    add auth reducer

---
+++ chat-frontend/src/store/reducers/auth.js
@@ -0,0 +1,23 @@
+import { LOGIN } from '../actions/auth';
+
+const initialState = {
+  user: {},
+  token: '',
+  isLoggedIn: false,
+};
+
+const authReducer = (state = initialState, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case LOGIN:
+      return {
+        ...state,
+        user: payload,
+        token: payload.token,
+        isLoggedIn: true,
+      };
+    default:
+      return state;
+  }
+};
