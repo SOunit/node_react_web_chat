5cadae4e15f2f6d47abf7ad613b668eb52c43bb3
SOunit <workbookwork7@gmail.com>
2021-09-18 13:57:09 -0700

    add navbar component

---
+++ chat-frontend/src/components/Chat/components/Navbar.js
@@ -0,0 +1,20 @@
+import { useSelector } from 'react-redux';
+import './Navbar.scss';
+
+const Navbar = () => {
+  const user = useSelector((state) => state.authReducer.user);
+
+  return (
+    <div id='navbar'>
+      <h2>Chat.io</h2>
+      <div id='profile-menu'>
+        <img src={user.avatar} alt='Avatar' />
+        <p>
+          {user.firstName} {user.lastName}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
