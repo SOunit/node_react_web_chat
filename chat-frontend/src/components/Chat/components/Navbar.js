da21690fb904c8b12a705ff43d857efec1f29af1
SOunit <workbookwork7@gmail.com>
2021-09-18 19:45:41 -0700

    add user update form

--- chat-frontend/src/components/Chat/components/Navbar.js
+++ chat-frontend/src/components/Chat/components/Navbar.js
@@ -11,6 +11,25 @@ const Navbar = () => {
   const [showProfileOptions, setShowProfileOptions] = useState(false);
   const [showProfileModal, setShowProfileModal] = useState(false);
 
+  const [firstName, setFirstName] = useState(user.firstName);
+  const [lastName, setLastName] = useState(user.lastName);
+  const [email, setEmail] = useState(user.email);
+  const [gender, setGender] = useState(user.gender);
+  const [password, setPassword] = useState('');
+  const [avatar, setAvatar] = useState('');
+
+  const submitForm = (e) => {
+    e.preventDefault();
+
+    const form = { firstName, lastName, email, gender, password, avatar };
+
+    const formData = new FormData();
+
+    for (const key in form) {
+      formData.push(key, form[key]);
+    }
+  };
+
   return (
     <div id='navbar'>
       <h2>Chat.io</h2>
@@ -45,9 +64,68 @@ const Navbar = () => {
 
         {showProfileModal && (
           <Modal click={() => setShowProfileModal(false)}>
-            <Fragment key='header'>Modal Header 1</Fragment>
-            <Fragment key='body'>Modal Body 2</Fragment>
-            <Fragment key='footer'>Modal Footer 3</Fragment>
+            <Fragment key='header'>
+              <h3 className='m-0'>Update profile</h3>
+            </Fragment>
+            <Fragment key='body'>
+              <form>
+                <div className='input-field mb-1'>
+                  <input
+                    placeholder='First name'
+                    onChange={(e) => setFirstName(e.target.value)}
+                    value={firstName}
+                    required='required'
+                    type='text'
+                  />
+                </div>
+                <div className='input-field mb-1'>
+                  <input
+                    placeholder='Last name'
+                    onChange={(e) => setLastName(e.target.value)}
+                    value={lastName}
+                    required='required'
+                    type='text'
+                  />
+                </div>
+                <div className='input-field mb-1'>
+                  <input
+                    placeholder='Email'
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                    required='required'
+                    type='text'
+                  />
+                </div>
+                <div className='input-field mb-1'>
+                  <select
+                    onChange={(e) => setGender(e.target.value)}
+                    value={gender}
+                    required='required'
+                  >
+                    <option value='male'>Male</option>
+                    <option value='female'>Female</option>
+                  </select>
+                </div>
+                <div className='input-field mb-2'>
+                  <input
+                    placeholder='Password'
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                    required='required'
+                    type='password'
+                  />
+                </div>
+                <div className='input-field mb-2'>
+                  <input
+                    onChange={(e) => setAvatar(e.target.files[0])}
+                    type='file'
+                  />
+                </div>
+              </form>
+            </Fragment>
+            <Fragment key='footer'>
+              <button className='btn-success'>UPDATE</button>
+            </Fragment>
           </Modal>
         )}
       </div>
