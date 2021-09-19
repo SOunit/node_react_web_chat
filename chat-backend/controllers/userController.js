8ab9988d3ee92ba8e319c240bd81411798b739b7
SOunit <workbookwork7@gmail.com>
2021-09-19 09:51:52 -0700

    add update user controller

--- chat-backend/controllers/userController.js
+++ chat-backend/controllers/userController.js
@@ -1,6 +1,25 @@
-const User = require('../models/user');
+const User = require('../models').User;
 const sequelize = require('sequelize');
 
-exports.update = (req, res) => {
-  return res.send('User controller');
+exports.update = async (req, res) => {
+  try {
+    const [rows, result] = await User.update(req.body, {
+      where: { id: req.user.id },
+      // to return values below
+      // 1. update count
+      // 2. updated users in array
+      returning: true,
+      // to execute hash hooks
+      individualHooks: true,
+    });
+
+    const user = result[0].get({ row: true });
+    // to use sequelize instance method when getiing avatar
+    user.avatar = result[0].avatar;
+    delete user.password;
+
+    return res.send(user);
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
 };
--- chat-backend/middleware/auth.js
+++ chat-backend/middleware/auth.js
@@ -11,7 +11,7 @@ exports.auth = (req, res, next) => {
 
   jwt.verify(token, config.appKey, (err, user) => {
     if (err) {
-      return res.status(401).json({ err });
+      return res.status(401).json({ error: err });
     }
 
     req.user = user;
