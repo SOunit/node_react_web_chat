63f2db33c1ac2f6e6dfeadcf481f17d86c06edf1
SOunit <workbookwork7@gmail.com>
2021-09-19 10:00:12 -0700

    add validate to update user

--- chat-backend/router/user.js
+++ chat-backend/router/user.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const { update } = require('../controllers/userController');
+const { rules: updateRules } = require('../validators/user');
 const { validate } = require('../validators');
 const { auth } = require('../middleware/auth');
 
-router.post('/update', [auth], update);
+router.post('/update', [auth, updateRules, validate], update);
 
 module.exports = router;
