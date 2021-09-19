9c1af6a523e02ce62c586b0d2a19e29d6be7371d
SOunit <workbookwork7@gmail.com>
2021-09-19 14:19:35 -0700

    add serving static file in uploads in api

--- chat-backend/index.js
+++ chat-backend/index.js
@@ -10,6 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(router);
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/uploads'));
 
 const port = config.appPort;
 app.listen(port, () => {
