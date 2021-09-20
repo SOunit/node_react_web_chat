0bb97cc267c19befb2bb65e822f884c52f8b5e2b
SOunit <workbookwork7@gmail.com>
2021-09-20 16:05:13 -0700

    add chat friend component

--- chat-frontend/src/components/Friend/Friend.js
+++ chat-frontend/src/components/Friend/Friend.js
@@ -1,11 +1,42 @@
 import './Friend.scss';
+import { useSelector } from 'react-redux';
+import { userStatus } from '../../utils/helpers';
 
 const Friend = ({ chat }) => {
+  const currentChat = useSelector((state) => state.chatReducer.currentChat);
+
+  const isChatOpen = () => {
+    return currentChat.id === chat.id ? 'open' : '';
+  };
+
+  const lastMessage = () => {
+    if (chat.Messages.length === 0) {
+      return '';
+    }
+
+    const message = chat.Messages[chat.Messages.length - 1];
+    return message.type === 'image' ? 'image uploaded' : message.message;
+  };
+
   return (
-    <div>
-      <p>
-        {chat.Users[0].firstName} {chat.Users[0].lastName}
-      </p>
+    <div className={`friend-list ${isChatOpen()}`}>
+      <div>
+        <img
+          width='40'
+          height='40'
+          src={chat.Users[0].avatar}
+          als='User avatar'
+        />
+        <div className='friend-info'>
+          <h4 className='m-0'>
+            {chat.Users[0].firstName} {chat.Users[0].lastName}
+          </h4>
+          <h5 className='m-0'>{lastMessage()}</h5>
+        </div>
+      </div>
+      <div className='friend-status'>
+        <span className={`online-status ${userStatus(chat.Users[0])}`}></span>
+      </div>
     </div>
   );
 };
