dcf10a4527d58a821b2c5ff0e69186aeb50fce03
SOunit <workbookwork7@gmail.com>
2021-09-19 21:02:53 -0700

    add create chat in api

--- chat-backend/controllers/chatController.js
+++ chat-backend/controllers/chatController.js
@@ -1,4 +1,5 @@
 const { Op } = require('sequelize');
+const { sequelize } = require('../models');
 const models = require('../models');
 const User = models.User;
 const Chat = models.Chat;
@@ -20,4 +21,67 @@ exports.index = async (req, res) => {
   });
 
   return res.send(user.Chats);
+};
+
+exports.create = async (req, res) => {
+  const { partnerId } = req.body;
+  const transaction = await sequelize.transaction();
+
+  try {
+    const user = await User.findOne({
+      where: { id: req.user.id },
+      include: [
+        {
+          model: Chat,
+          where: { type: 'dual' },
+          include: [
+            {
+              model: ChatUser,
+              where: { userId: partnerId },
+            },
+          ],
+        },
+      ],
+    });
+
+    if (user && user.Chats.length > 0) {
+      return res.status(403).json({
+        status: 'Error',
+        message: 'Chat with this user already exists!',
+      });
+    }
+
+    const chat = await Chat.create(
+      { type: 'dual' },
+      { transaction: transaction }
+    );
+
+    await ChatUser.bulkCreate(
+      [
+        {
+          chatId: chat.id,
+          userId: req.user.id,
+        },
+        {
+          chatId: chat.id,
+          userId: partnerId,
+        },
+      ],
+      { transaction: transaction }
+    );
+    await transaction.commit();
+
+    const chatNew = await Chat.findOne({
+      where: { id: chat.id },
+      include: [
+        { model: User, where: { [Op.not]: { id: req.user.id } } },
+        { model: Message },
+      ],
+    });
+
+    return res.send(chatNew);
+  } catch (err) {
+    await transaction.rollback();
+    return res.status(500).json({ status: 'Error', message: err.message });
+  }
 };
--- chat-backend/router/chat.js
+++ chat-backend/router/chat.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
-const { index } = require('../controllers/chatController');
+const { index, create } = require('../controllers/chatController');
 const { validate } = require('../validators');
 const { auth } = require('../middleware/auth');
 
 router.get('/', [auth], index);
+router.post('/create', [auth], create);
 
 module.exports = router;
