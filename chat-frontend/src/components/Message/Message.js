09067620126141af2869f6fd9c105f13af0ff98b
SOunit <workbookwork7@gmail.com>
2021-09-21 12:09:50 -0700

    add user name to each message

--- chat-frontend/src/components/Message/Message.js
+++ chat-frontend/src/components/Message/Message.js
@@ -20,6 +20,11 @@ const Message = ({ user, chat, message, index }) => {
       <div
         className={message.fromUserId === user.id ? 'owner' : 'other-person'}
       >
+        {message.fromUserId !== user.id ? (
+          <h6 className='m-0'>
+            {message.User.firstName} {message.User.lastName}
+          </h6>
+        ) : null}
         {message.type === 'text' ? (
           <p className='m-0'>{message.message}</p>
         ) : (
