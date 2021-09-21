09c492c059819709e69853f89605e499395d4281
SOunit <workbookwork7@gmail.com>
2021-09-20 17:07:07 -0700

    add messenger component

--- chat-frontend/src/components/FriendList/FriendList.js
+++ chat-frontend/src/components/FriendList/FriendList.js
@@ -12,7 +12,7 @@ const FriendList = () => {
   };
 
   return (
-    <div id='friends'>
+    <div id='friends' className='shadow-light'>
       <div id='title'>
         <h3 className='m-0'>Friends</h3>
         <button>ADD</button>
